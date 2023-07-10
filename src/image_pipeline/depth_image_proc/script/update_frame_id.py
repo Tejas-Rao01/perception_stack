551b362244c5a72b37880b6d202977bcf57a5261
Tejas Rao <rao.tejas01@gmail.com>
2023-07-10 14:47:34 +0530

    elevation_mapping

--- src/image_pipeline/depth_image_proc/script/update_frame_id.py
+++ src/image_pipeline/depth_image_proc/script/update_frame_id.py
@@ -11,12 +11,12 @@ class update_frame_id:
     def __init__(self):
         self.image = Image()
         #subscribe to your specific sensors
-        self.sub_raw = rospy.Subscriber("/carla/ego_vehicle/rgb_front/image", Image, self.callback_raw)
-        self.sub_info = rospy.Subscriber("/carla/ego_vehicle/rgb_front/camera_info", CameraInfo, self.callback_info)
+        self.sub_raw = rospy.Subscriber("/d435/color/image_raw", Image, self.callback_raw)
+        self.sub_info = rospy.Subscriber("/d435/color/camera_info", CameraInfo, self.callback_info)
         self.pub_raw = rospy.Publisher("/rgb/image_rect_color", Image, queue_size = 1)
         self.pub_info = rospy.Publisher("/rgb/camera_info", CameraInfo, queue_size = 1)
     def callback_raw(self, message):
-        message.header.frame_id = "ego_vehicle/depth_front"
+        message.header.frame_id = "d435_depth_optical_frame"
         self.pub_raw.publish(message)
     def callback_info(self, message):
         message.header.frame_id = "ego_vehicle/depth_front"
