c0d0b4baf0390300d63c96b3663acbf1f3f6cef2
Kartikgar <gargkartikxx@gmail.com>
2023-07-17 03:44:50 +0530

    Added mapping functionality

---
+++ turtlebot_mapping.sh
@@ -0,0 +1,14 @@
+#/usr/bin/bash
+
+source devel/setup.bash
+on_interrupt ()
+{
+    echo "saving map"
+    rosrun octomap_server octomap_saver -f mapfile5.ot # subsequent ctrl+c will end this and the script
+
+}
+export TURTLEBOT3_MODEL=burger
+roslaunch turtlebot3_gazebo mapping.launch & 
+trap "on_interrupt" 20
+
+wait
--- turtlebot_octomapping.sh
+++ turtlebot_octomapping.sh
@@ -1,6 +1,14 @@
-#/usr/bin/bash 
+#/usr/bin/bash
 
 source devel/setup.bash
-export TURTLEBOT3_MODEL=burger
-roslaunch turtlebot3_gazebo octomapping.launch
+on_interrupt ()
+{
+    echo "saving map"
+    rosrun octomap_server octomap_saver -f mapfile3.ot # subsequent ctrl+c will end this and the script
 
+}
+export TURTLEBOT3_MODEL=burger
+roslaunch turtlebot3_gazebo octomapping.launch & 
+trap "on_interrupt" 20
+
+wait
